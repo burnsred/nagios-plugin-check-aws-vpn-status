234018c55b3415e0b013afa2194a9b6a98c0ec15
Michael Bertolacci <m.bertolacci@gmail.com>
2015-08-07 17:03:00 +0800

    Make access keys optional to support the use of IAM roles

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nagios-plugin-check-aws-vpn-status',
-    version='0.0.3',
+    version='0.0.4',
     packages=[],
     scripts=['bin/check-aws-vpn-status'],
     include_package_data=True,
